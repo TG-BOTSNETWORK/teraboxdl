48e28f18a2c43a19f0f03e07fcc24913ca00abc3
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2025-03-08 23:53:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     
 setup(
     name='teraboxdl',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     url='https://github.com/TG-BOTSNETWORK/teraboxdl',
     author='Santhosh',
